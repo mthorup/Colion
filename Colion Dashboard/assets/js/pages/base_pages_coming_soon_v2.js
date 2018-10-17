bbb56e57cef38f691bc527ad56bf2358d9e9c35d
Mikkel <mikkel.thorup1@gmail.com>
2018-10-17 23:18:33 +0200

    Text and date changed

--- Colion Dashboard/assets/js/pages/base_pages_coming_soon_v2.js
+++ Colion Dashboard/assets/js/pages/base_pages_coming_soon_v2.js
@@ -7,7 +7,7 @@
 var BasePagesComingSoonv2 = function() {
     // Init Countdown.js, for more examples you can check out https://github.com/hilios/jQuery.countdown
     var initCounter = function(){
-        jQuery('.js-countdown').countdown((new Date().getFullYear() + 1) + '/02/20', function(event) {
+        jQuery('.js-countdown').countdown((new Date().getFullYear()) + '/12/20', function(event) {
             jQuery(this).html(event.strftime('<div class="row items-push text-center">'
                     + '<div class="col-xs-6 col-sm-3"><div class="font-s48 font-w700 text-white">%-D</div><div class="font-w600 text-gray">DAYS</div></div>'
                     + '<div class="col-xs-6 col-sm-3"><div class="font-s48 font-w700 text-white">%H</div><div class="font-w600 text-gray">HOURS</div></div>'
